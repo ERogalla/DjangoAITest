d3f9ca64124c6a25d8f2ba71953754c5ce9ad89c
Emilio <erogalla@utexas.edu>
2019-12-26 10:42:35 -0600

    predicted and tested model

--- aimodel/bankloanmodel.py
+++ aimodel/bankloanmodel.py
@@ -3,11 +3,43 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 from imblearn.over_sampling import SMOTE
-from sklearn.preprosessing import MinMaxScalar
+from sklearn.preprocessing import MinMaxScaler
 import warnings
 from collections import Counter
 warnings.filterwarnings('ignore')
 import seaborn as sns
 import matplotlib.pyplot as plt
 from keras import Sequential
-from kears.layers import Dense
+from keras.layers import Dense
+
+df = pd.read_csv('bankloan.csv')
+df = df.dropna()
+df.isna().any() # adds the seperate collums for T/F questions
+df = df.drop('Loan_ID', axis=1)
+df['LoanAmount'] = (df['LoanAmount']*1000).astype(int)
+
+pre_y = df['Loan_Status']
+pre_X = df.drop('Loan_Status', axis=1)
+
+dm_X = pd.get_dummies(pre_X)
+dm_y = pre_y.map(dict(Y=1, N=0))
+
+smote = SMOTE(sampling_strategy='minority')
+X1, y = smote.fit_sample(dm_X, dm_y)
+sc = MinMaxScaler()
+X = sc.fit_transform(X1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+
+classifier = Sequential()
+classifier.add(Dense(200, activation='relu', kernel_initializer='random_normal', input_dim=X_test.shape[1]))
+classifier.add(Dense(400, activation='relu', kernel_initializer='random_normal'))
+classifier.add(Dense(4, activation='relu', kernel_initializer='random_normal'))
+classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+classifier.fit(X_train, y_train, batch_size=20, epochs=50, verbose=0)
+eval_model=classifier.evaluate(X_train, y_train)
+
+y_pred=classifier.predict(X_test)
+y_pred=(y_pred>0.52)
+
